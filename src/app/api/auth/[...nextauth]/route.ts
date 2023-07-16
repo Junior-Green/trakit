30cca91ea631c78a9cab0530b049a8a8489606cd
Junior Green <juniorgreen9185@hotmail.com>
2023-07-16 16:08:49 -0400

    fix edge function configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,13 @@
-import {randomBytes, randomUUID} from "crypto";
-import NextAuth, {NextAuthOptions} from "next-auth";
+import { randomBytes, randomUUID } from "crypto";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import RedditProvider from "next-auth/providers/reddit";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
 import clientPromise from "@/src/database/mongo-client";
-import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
-export const config = {
-    runtime: 'edge',
-  };
+export const runtime = 'edge'
 
 const REDDIT_ID = process.env.REDDIT_ID;
 const REDDIT_SECRET = process.env.REDDIT_SECRET;
@@ -90,7 +88,7 @@ export const authOptions: NextAuthOptions = {
         }
     },
     callbacks: {
-        async session({session, token, user}) {
+        async session({ session, token, user }) {
             // Send properties to the client, like an access_token and user id from a provider.
             session.user.id = user.id;
             return session;
@@ -102,4 +100,4 @@ export const authOptions: NextAuthOptions = {
 };
 
 const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
